13f6047ca37fec7506c43aabc488706294cdb294
Dillon Bourne <dillon_bourne82@live.com>
2019-09-24 15:46:32 -0400

    run changes

--- CMAES/Experiments_CMAES.jl
+++ CMAES/Experiments_CMAES.jl
@@ -33,12 +33,12 @@ function runexpr(exprName::String; reps = 20, outputPath = "", summary = true, m
 
 					if summary
 						write_final(ipop; prefixNames = prefixNames, prefixValues = prefixValues,
-						            	  initialize = firstTime, path = outputPath, fileName = "allfns_test_final$exprName")
+						            	  initialize = firstTime, path = outputPath, fileName = "allfns_50runs_AGS_final$exprName")
 					end
 
 					if monitored
 						write_run(ipop, sys, f; prefixNames = prefixNames, prefixValues = prefixValues,
-					   			    	     	initialize = firstTime, path = outputPath, fileName = "allfns_test_run$exprName", sep = ",")
+					   			    	     	initialize = firstTime, path = outputPath, fileName = "allfns_50runs_AGS_run$exprName", sep = ",")
 					end
 
 					firstTime = false
@@ -49,4 +49,4 @@ function runexpr(exprName::String; reps = 20, outputPath = "", summary = true, m
 end
 
 expr_path = "$(base_path)/Experiments/fixedbudgetruns"
-runexpr("#dual-center", reps = 5, outputPath = expr_path, monitored = true)
+runexpr("#dual-center", reps = 50, outputPath = expr_path, monitored = true)
