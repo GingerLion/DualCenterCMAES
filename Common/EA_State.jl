d483f9c36492e8ece3768d28e055eece7de0fce5
Dillon Bourne <dillon_bourne82@live.com>
2019-11-06 11:51:48 -0500

    tightened up found_() condition

--- Common/EA_State.jl
+++ Common/EA_State.jl
@@ -105,16 +105,16 @@ same(member::Tuple, state::State) = bestfitness(member) == bestfitness(state)
 
 function found(state::State, fit::Fitness)
 	if found_(state)
-	 	abs(bestfitoverall(state) - fit.optimalValue) <= fit.epsilon && better_overall(state, best_overall_(state))
+	 	abs(bestfitoverall(state) - fit.optimalValue) == fit.epsilon && better_overall(state, best_overall_(state))
 	else
-		abs(bestfitoverall(state) - fit.optimalValue) <= fit.epsilon
+		abs(bestfitoverall(state) - fit.optimalValue) == fit.epsilon
 	end
 end
 function found_(state::State, fit::Fitness)
 	if found(state)
-		abs(bestfitoverall_(state) - fit.optimalValue) <= fit.epsilon && better_overall_(state, best_overall(state))
+		abs(bestfitoverall_(state) - fit.optimalValue) == fit.epsilon && better_overall_(state, best_overall(state))
 	else
-		abs(bestfitoverall_(state) - fit.optimalValue) <= fit.epsilon
+		abs(bestfitoverall_(state) - fit.optimalValue) == fit.epsilon
 	end
 end
 
@@ -161,5 +161,6 @@ function found!_(state::State, fit::Fitness)
 		println("Shadow found solution at gen = $(currentgen_(state))")
 		state.status_shadow = :found
 		state.stopEvals_ = true
+		println("-------FOUND------")
 	end
 end
